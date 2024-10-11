7df03b6666fec7e928c97f122271e767d105a572
bi-Tech2 <141980137+bi-Tech2@users.noreply.github.com>
2024-10-11 14:29:20 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,38 @@ const accordionAction = document.querySelectorAll("[data-accordion-action]");
 const toggleAccordion = function () { this.classList.toggle("active"); }
 
 addEventOnElem(accordionAction, "click", toggleAccordion);
+
+
+
+
+
+
+$(document).ready(function() {
+  $('.testimonial-slider').slick({
+      autoplay: true,
+      autoplaySpeed: 1000,
+      speed: 600,
+      draggable: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false,
+      responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+          }
+      ]
+  });
+}); 
